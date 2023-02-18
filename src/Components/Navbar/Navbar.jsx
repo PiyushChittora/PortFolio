d4b6adb4bb27b1c5a98921437f10ea590380c9b8
PiyushChittora <piyushchittora35246@gmail.com>
2023-02-18 17:01:19 +0530

    nav links fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,13 +9,13 @@ export default function Navbar() {
       <div className="nav">
         <a href="/" className="name">Piyush Chittora</a>
         <div className="connect">
-          <a href="https://www.linkedin.com/in/piyush-chittora-aba673203/">
+          <a href="https://www.linkedin.com/in/piyush-chittora-aba673203/" target="_blank">
             <img src={linkedin} alt="" className="linkedin" />
           </a>
-          <a href="https://github.com/PiyushChittora">
+          <a href="https://github.com/PiyushChittora" target="_blank">
             <img src={github} alt="" className="github" />
           </a>
-          <a href="https://drive.google.com/file/d/1arLr2pa8f2FNuHa-2m0ri8Ixl_cEMqjE/view?usp=sharing" className="resume">
+          <a href="https://drive.google.com/file/d/1arLr2pa8f2FNuHa-2m0ri8Ixl_cEMqjE/view?usp=sharing" className="resume" target="_blank">
             <div className="resume-text">My Resume</div>
           </a>
         </div>
